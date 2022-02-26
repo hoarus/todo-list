f9cc5ac68be5015d1a72f5f4d071232990d373ab
hoarus <hoarus@gmail.com>
2022-02-26 14:51:40 +1000

    Style pop-up and enable creation of task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,47 @@ import './stylesheets/normalise.css'
 import './stylesheets/style.css';
 
 
+let newTaskButton = document.querySelector(".new-task");
+let taskForm = document.querySelector(".task-form-container");
+let closeTaskForm = document.querySelector(".close-task-form");
+let toDoContainer = document.querySelector(".to-do-container");
+let createTaskButton = document.querySelector(".create-task");
+let pageWrapper = document.querySelector(".page-wrapper");
+//Write code to make add new task
 
-console.log('I get called from print.js!');
+// Event Listeners
+newTaskButton.addEventListener("click", () => {
+  displayItem(taskForm);
+  pageWrapper.classList.add("dimmed");
+});
+
+closeTaskForm.addEventListener("click", () => {
+  hideItem(taskForm);
+  pageWrapper.classList.remove("dimmed");
+});
+
+createTaskButton.addEventListener("click", () => {
+  createTask();
+});
+
+//Functions
+
+function displayItem(item) {
+  item.classList.remove("hidden");
+}
+
+function hideItem(item) {
+  item.classList.add("hidden");
+}
+
+function createTask(){
+  let newTask = generateTask();
+  toDoContainer.appendChild(newTask);
+}
+
+function generateTask(){
+  const newTask = document.createElement("div");
+  newTask.setAttribute("class", "task");
+  newTask.textContent = "Test";
+  return newTask;
+}
